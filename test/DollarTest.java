a5e3370b7ab9b69140e0020516bfcd2a8ba6aa8c
vnqthai <vnqthai@users.noreply.github.com>
2017-07-12 13:32:55 +0700

    Add example for chapter 11

--- test/DollarTest.java
+++ test/DollarTest.java
@@ -14,18 +14,9 @@ public class DollarTest {
   }
 
   @Test
-  public void testFrancMultiplication() {
-    Money five = Money.franc(5);
-    assertEquals(Money.franc(10), five.times(2));
-    assertEquals(Money.franc(15), five.times(3));
-  }
-
-  @Test
   public void testEquality() {
     assertTrue(Money.dollar(5).equals(Money.dollar(5)));
     assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-    assertTrue(Money.franc(5).equals(Money.franc(5)));
-    assertFalse(Money.franc(5).equals(Money.franc(6)));
     assertFalse(Money.franc(5).equals(Money.dollar(5)));
   }
 
@@ -35,8 +26,4 @@ public class DollarTest {
     assertEquals("CHF", Money.franc(1).currency());
   }
 
-  @Test
-  public void testDifferentClassEquality() {
-    assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
-  }
 }
