38ee9efbe77d890d56c38c2c887d591545e42700
vnqthai <vnqthai@users.noreply.github.com>
2017-07-13 11:07:49 +0700

    Add example for chapter 16

--- main/Expression.java
+++ main/Expression.java
@@ -3,4 +3,5 @@ package main;
 public interface Expression {
   public Money reduce(Bank bank, String to);
   public Expression plus(Expression addend);
+  public Expression times(int multiplier);
 }
--- main/Sum.java
+++ main/Sum.java
@@ -10,7 +10,11 @@ public class Sum implements Expression {
   }
 
   public Expression plus(Expression addend) {
-    return null;
+    return new Sum(this, addend);
+  }
+
+  public Expression times(int multiplier) {
+    return new Sum(augend.times(multiplier), addend.times(multiplier));
   }
 
   public Money reduce(Bank bank, String to) {
